7ba4abb57657f9d6709402f6de510bc7f29ae1d9
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-08-23 11:19:00 +0000

    add secret key in env

--- events_project/settings.py
+++ events_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b7l0_u2f(3%+)g&8tf384r6-cik2xncd4sktag@rk20*j&7(hx'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
