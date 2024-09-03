3fd10f1ce0da6fb9c69891f611a184fddb04ab49
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-09-03 14:55:11 +0000

    Make the events a link to their own page

--- event/urls.py
+++ event/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('event/', views.EventList.as_view(), name='home'), #This pattern tells Django to look in the event app URL file for any blog urlpatterns.
+    path('<slug:slug>/', views.addevent_detail, name='addevent_detail'),
 ]
--- event/views.py
+++ event/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic 
 from .models import AddEvent
 
@@ -10,6 +10,29 @@ class EventList(generic.ListView):
     Displaying events from the AddEvent model
     """
     queryset = AddEvent.objects.all() # Gets all 'AddEvent' objects from the database and sends them to the template.
-    # template_name = "addevent_list.html" # Specifies the template that should be rendered
     template_name = "event/index.html"
     paginate_by = 6
+    # paginate by 6 tells Django to display 6 posts at a time
+
+def addevent_detail(request, slug):
+    """
+    Display an individual :model:`event.AddEvent`.
+
+    **Context**
+
+    ``addevent``
+        An instance of :model:`event.AddEvent`.
+
+    **Template:**
+
+    :template:`event/addevent_detail.html`
+    """
+
+    queryset = AddEvent.objects.filter(status=1)
+    addevent = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "event/addevent_detail.html",
+        {"event": addevent},
+    )
