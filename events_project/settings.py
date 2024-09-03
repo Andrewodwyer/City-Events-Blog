6b39df32327a46a92fb61f5db6f1af5cdf74ef48
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-09-03 15:13:23 +0000

    added load static file to index.html so images from static directory would load

--- events_project/settings.py
+++ events_project/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates') # templates is the top level
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-andrewodwye-cityeventsb-c2d8j24zojp.ws.codeinstitute-ide.net', '.herokuapp.com']
 
