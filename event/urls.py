4d67955a254dfc9025adee05c6f079ee3880c17a
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-09-05 11:20:24 +0000

    Allauth added, css style for new login pages

--- event/urls.py
+++ event/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('event/', views.EventList.as_view(), name='home'), #This pattern tells Django to look in the event app URL file for any blog urlpatterns.
+    path('', views.EventList.as_view(), name='home'), #This pattern tells Django to look in the event app URL file for any blog urlpatterns.
     path('calendar/', views.calendar_view, name='calendar'),  # Path for the calendar page
     path('api/events/', views.get_events, name='get_events'),  # API to fetch events from the database
     path('event/<slug:slug>/', views.addevent_detail, name='addevent_detail'),
--- events_project/urls.py
+++ events_project/urls.py
@@ -18,9 +18,10 @@ from django.contrib import admin
 from django.urls import path, include #include allows us to import and use another urls.py file
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), #admins url
+    path("accounts/", include("allauth.urls")),
     path('summernote/', include('django_summernote.urls')),
-    path("", include("event.urls"), name="event-urls"), 
+    path("", include("event.urls"), name="event-urls"), #events page url including, paginated events and calender page
 ]
 """
 "add-name" is now the top level of the url path, the app name will now come after this
