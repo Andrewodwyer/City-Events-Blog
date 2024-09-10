15a25db61bdc1816600acb947f3eb3b71d72f222
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-09-10 15:34:13 +0000

    Working on navbar, no change

--- event/models.py
+++ event/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from cloudinary.models import CloudinaryField
+from cloudinary.models import CloudinaryField #use cloudinary for images
 from django.utils import timezone #local time 
 
 """
